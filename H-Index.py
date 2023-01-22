e571dd1ea6d91d4b8a7ff417a492f71b3d2827ac
GeraldTgit <117510768+GeraldTgit@users.noreply.github.com>
2023-01-22 17:17:20 +0800

    Update H-Index.py

--- H-Index.py
+++ H-Index.py
@@ -12,14 +12,46 @@ def hIndex(publications):
 
   publications.sort(reverse=True)
 
-  for k, v in enumerate(publications):
+  for k, v in enumerate(publications, start=1):
 
-    if k + 1 == v:
+    #publications[k-1] refers to v
 
-        return v
+    if k <= v and k-1 < publications[k-2] and k+1 > publications[k]:
+
+      return k
 
 
 
-print(hIndex([5, 4, 2, 1, 3]))
+print(hIndex([5, 4, 3, 2, 1]))
 
-# 3
+#             1, 2, 3, 4, 5
+
+#Answer       3
+
+
+
+print(hIndex([10,15,9,5,1]))
+
+#             2, 1, 3,4,5
+
+#Answer       4
+
+
+
+print(hIndex([263,84,61,24,15,9,8,5]))
+
+#             1,  2, 3, 4, 5, 6,7,8
+
+#Answer       7
+
+
+
+print(hIndex([87,70,46,32,19,15,10,9,8,6,4,1]))
+
+#Answer       8
+
+
+
+print(hIndex([33,30,20,15,7,6,5,4]))
+
+#Answer       6
